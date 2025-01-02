418b0afc3bd900f85f15d4951db3b51970d2dd2a
winsanmwtv <wntv2020@gmail.com>
2025-01-02 22:25:29 +0700

    หน้าเปล่าๆ

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -13,5 +13,44 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+      require('daisyui'),
+  ],
+  daisyui: {
+    themes: [
+      {
+        cityrail_light: {
+          "primary": "#5a9dff",
+          "secondary": "#f595ff",
+          "accent": "#2eecb6",
+          "neutral": "#2b2b2b",
+          "base-100": "#ffffff",
+          "info": "#5a9dff",
+          "success": "#067354",
+          "warning": "#ffb906",
+          "error": "#de2727",
+        },
+        cityrail_dark: {
+          "primary": "#5a9dff",
+          "secondary": "#f595ff",
+          "accent": "#2eecb6",
+          "neutral": "#2b2b2b",
+          "base-100": "#151515",
+          "info": "#5a9dff",
+          "success": "#067354",
+          "warning": "#ffb906",
+          "error": "#de2727",
+        },
+      },
+      "light",
+      "dark",
+    ], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "cityrail_dark", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
+  },
 };
