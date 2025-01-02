2d7e174abc01101cd2779f640d9f2c3bef07c9ae
winsanmwtv <wntv2020@gmail.com>
2025-01-02 22:53:15 +0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -6,9 +6,9 @@ export default function Home() {
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
           <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
               <Image
-                  src = "/under_development.png"
-                  alt = "Under Development Dialog"
-                  width = {600}
+                  src = "/cityrail.png"
+                  alt = "City Rail Logo"
+                  width = {300}
                   height = {500}
                   priority
               ></Image>
