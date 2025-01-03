ee118e88fcb72720810d29873d8a64214f5a4815
winsanmwtv <wntv2020@gmail.com>
2025-01-03 17:23:40 +0700

    404

--- src/app/loading.js
+++
@@ -1,9 +0,0 @@
-export default function Loading() {
-    return (
-        <div className="bg-base-100 text-base-content flex items-center justify-center min-h-screen">
-            <div className="text-center">
-                <h1 className="text-4xl font-bold">เว็บไซต์กำลังโหลด กรุณารอสักครู่</h1>
-            </div>
-        </div>
-    );
-}
--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,9 +1,8 @@
-export default function NotFound() {
+export default function Loading() {
     return (
-        <div className="bg-base-100 text-base-content flex items-center justify-center min-h-screen">
+        <div className="bg-base-100 text-base-content flex items-center justify-center min-h-screen px-4">
             <div className="text-center">
-                <h1 className="text-4xl font-bold">404 | ไม่พบหน้าที่ร้องขอ</h1>
-                <p className="mt-2 text-lg">มีบางอย่างผิดพลาดในลิงก์ URL หรือไม่?</p>
+                <h1 className="text-2xl sm:text-4xl font-bold">เว็บไซต์กำลังโหลด กรุณารอสักครู่</h1>
             </div>
         </div>
     );
