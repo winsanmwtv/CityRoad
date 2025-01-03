f88469413c43d6ed1867959ae2a03660b2e43797
winsanmwtv <wntv2020@gmail.com>
2025-01-03 17:33:14 +0700

    โดนหาว่าเล่นเกมเฉยเลย

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -2,14 +2,14 @@ import link from "next/link"
 
 const Footer = () => {
     return (
-      <footer className="bg-secondary text-white">
+      <footer className="bg-secondary text-secondary-content">
           <div className='max-w-7xl mx-auto px-2 py-12 sm:px-6 lg:px-8'>
               <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
                   <h3 className="text-lg font-bold mb-4">CityRoad</h3>
               </div>
               <div className="text-sm mt-8">
                   <p>
-                      การรายงานข้อมูลมาจากภาคประชาชน อาจมีการผิดพลาดของข้อมูลได้
+                      คำสงวนสิทธิ์: ข้อมูลที่ได้จากการสำรวจและรายงานจากภาคประชาชนอาจมีข้อจำกัดในด้านความถูกต้องแม่นยำ
                   </p>
                   <br/>
                   <p>
--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,9 +1,9 @@
 export default function NotFound() {
     return (
-        <div className="bg-accent text-accent-content flex items-center justify-center min-h-screen px-4">
+        <div className="bg-base-100 text-base-content flex items-center justify-center min-h-screen px-4">
             <div className="text-center">
-                <h1 className="text-3xl sm:text-4xl font-bold">404 | Page not found</h1>
-                <p className="mt-2 text-base sm:text-lg">Is there any mistake in the URL?</p>
+                <h1 className="text-3xl sm:text-4xl font-bold">404 | ไม่พบหน้าที่ร้องขอ</h1>
+                <p className="mt-2 text-base sm:text-lg">มีบางอย่างผิดพลาดในลิงก์ URL หรือไม่?</p>
             </div>
         </div>
     );
