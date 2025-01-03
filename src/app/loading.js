54131f7b378e4b4e955bda1ab7d231c3f9e08cd9
winsanmwtv <wntv2020@gmail.com>
2025-01-03 17:21:16 +0700

    404 Page Not Found,

---
+++ src/app/loading.js
@@ -0,0 +1,9 @@
+export default function Loading() {
+    return (
+        <div className="bg-base-100 text-base-content flex items-center justify-center min-h-screen">
+            <div className="text-center">
+                <h1 className="text-4xl font-bold">เว็บไซต์กำลังโหลด กรุณารอสักครู่</h1>
+            </div>
+        </div>
+    );
+}
---
+++ src/app/not-found.js
@@ -0,0 +1,10 @@
+export default function NotFound() {
+    return (
+        <div className="bg-base-100 text-base-content flex items-center justify-center min-h-screen">
+            <div className="text-center">
+                <h1 className="text-4xl font-bold">404 | ไม่พบหน้าที่ร้องขอ</h1>
+                <p className="mt-2 text-lg">มีบางอย่างผิดพลาดในลิงก์ URL หรือไม่?</p>
+            </div>
+        </div>
+    );
+}
